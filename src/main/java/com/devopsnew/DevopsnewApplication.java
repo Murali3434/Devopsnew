cdc5354e461b259c87c533861f9b62f44be688ee
Murali3434 <muralihsr1994@gmail.com>
2025-05-19 16:35:29 +0530

    first commit

---
+++ src/main/java/com/devopsnew/DevopsnewApplication.java
@@ -0,0 +1,13 @@
+package com.devopsnew;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevopsnewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevopsnewApplication.class, args);
+	}
+
+}
